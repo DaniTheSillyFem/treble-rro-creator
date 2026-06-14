df221f54b91ebd950190c33e0026d65dcfca4466
uzbforce <oktamovazizbek2010@gmail.com>
2026-06-14 10:55:10 +0500

    Try to combine ARM with main

--- build.sh
+++ build.sh
@@ -31,7 +31,6 @@
 # =============================================================================
 
 set -e
-set -o pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
